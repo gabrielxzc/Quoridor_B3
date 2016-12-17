3b8c60e612e01eec79ac5abeac73172d829db71c
gabrielxzc <codrincojocaru@yahoo.com>
2016-12-17 16:26:27 +0200

    MeniuAlfa

--- Quoridor_GAME/Quoridor_GAME/main.cpp
+++ Quoridor_GAME/Quoridor_GAME/main.cpp
@@ -5,18 +5,13 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+SDL_Window *mainMenu()
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	
 	SDL_Window *window = nullptr;
-	window = SDL_CreateWindow("Learning SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-
-	bool isRunning = true;
-	SDL_Event event;
+	window = SDL_CreateWindow("Quoridor", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
 
 	// Setup renderer
-	SDL_Renderer* renderer = NULL;
+	SDL_Renderer* renderer = nullptr;
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 	// Set render color to red ( background will be rendered in this color )
@@ -25,48 +20,58 @@ int main(int argc, char *argv[])
 	// Clear winow
 	SDL_RenderClear(renderer);
 
-	// Creat a rect at pos ( 50, 50 ) that's 50 pixels wide and 50 pixels high.
-	SDL_Rect start;
-	start.x = 250;
-	start.y = 150;
-	start.w = 150;
-	start.h = 50;
+	// Create 3 rects for the menu buttons
 	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	SDL_Rect start = { 325, 150, 150, 50 }, info = { 325, 250, 150, 50 }, exit = { 325, 350, 150, 50 };
 	SDL_RenderFillRect(renderer, &start);
-
-	SDL_Rect info;
-	info.x = 250;
-	info.y = 250;
-	info.w = 150;
-	info.h = 50;
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 	SDL_RenderFillRect(renderer, &info);
-
-	SDL_Rect exit;
-	exit.x = 250;
-	exit.y = 350;
-	exit.w = 150;
-	exit.h = 50;
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 	SDL_RenderFillRect(renderer, &exit);
-
 
 	// Render the rect to the screen
 	SDL_RenderPresent(renderer);
+
+	return window;
+}
+
+SDL_Window *gameMenu()
+{
+	// Aici Cristinel
+}
+
+SDL_Window *instructionsMenu()
+{
+	// Aici Ionica
+}
+
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	
+	SDL_Window *window = nullptr;
+	bool isRunning = true;
+	SDL_Event event;
+
+	window = mainMenu();
 
 	while (isRunning)
 	{
 		while (SDL_PollEvent(&event))
 		{
-			if (event.type == SDL_QUIT)
+			if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE)
 				isRunning = false;
-			switch (event.key.keysym.sym)
+			if () // Buton start - Cristi
 			{
-			case SDLK_ESCAPE:
+				window = gameMenu();
+				window = mainMenu();
+			}
+			if () // Buton instructiuni - Ioana
+			{
+				window = instructionsMenu();
+				window = mainMenu();
+			}
+			if () // Buton exit - Codrin
+			{
 				isRunning = false;
-				break;
-			default:
-				break;
 			}
 		}
 	}

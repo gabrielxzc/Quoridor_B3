4e7d1efd1a29942c7ecbe3c8141641c0edc480e0
gabrielxzc <codrincojocaru@yahoo.com>
2016-12-25 18:10:45 +0200

    Menu, player vs player only moving

--- Quoridor_GAME/Quoridor_GAME/main.cpp
+++ Quoridor_GAME/Quoridor_GAME/main.cpp
@@ -2,12 +2,24 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
+#define p1XS 382
+#define p1YS 504
+#define p2XS 382
+#define p2YS 56
+#define moveUpDown 56
+#define moveLeftRight 59
+#define squareL 35
 
 using namespace std;
 
 SDL_Window *mainWindow = nullptr;
 SDL_Renderer *mainRenderer = nullptr;
 bool isRunning = true; // Inchide tot programul
+bool playerOneTurn = true;
+short playerOneWalls = 10;
+short playerTwoWalls = 10;
+int p1X, p1Y, p2X, p2Y;
+
 
 void addImageToRenderer(const char *file,int x, int y, int w, int h)
 {
@@ -23,7 +35,7 @@ void addImageToRenderer(const char *file,int x, int y, int w, int h)
 	SDL_DestroyTexture(texture);
 }
 
-void createPlayTable(SDL_Event event)
+void createPlayTable()
 {
 	SDL_SetRenderDrawColor(mainRenderer, 255, 255, 255, 255);
 
@@ -31,6 +43,18 @@ void createPlayTable(SDL_Event event)
 
 	addImageToRenderer("images/gameBoard.JPG", 0, 0, 800, 600);
 	addImageToRenderer("images/backButton.PNG", 700, 550, 70, 40);
+	addImageToRenderer("images/playerOne.PNG", p1X, p1Y, 35, 35);
+	addImageToRenderer("images/playerTwo.PNG", p2X, p2Y, 35, 35);
+
+	SDL_RenderPresent(mainRenderer);
+}
+
+void highlightPossibleMoves(int X, int Y)
+{
+	if (X - moveLeftRight >= p1XS - (4*moveLeftRight)) addImageToRenderer("images/highlightedSquare.PNG", X - moveLeftRight, Y, 35, 35);
+	if (X + moveLeftRight <= p1XS + (4*moveLeftRight)) addImageToRenderer("images/highlightedSquare.PNG", X + moveLeftRight, Y, 35, 35);
+	if (Y + moveUpDown <= p1YS ) addImageToRenderer("images/highlightedSquare.PNG", X, Y + moveUpDown, 35, 35);
+	if (Y - moveUpDown >= p2YS ) addImageToRenderer("images/highlightedSquare.PNG", X, Y - moveUpDown, 35, 35);
 
 	SDL_RenderPresent(mainRenderer);
 }
@@ -43,7 +67,7 @@ int playingAgainstComputer()
 	while (isRunning)
 		while (SDL_PollEvent(&event))
 		{
-			createPlayTable(event);
+			createPlayTable();
 
 			if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > 700 && event.motion.x < 770 && event.motion.y > 550 && event.motion.y < 590)
 				return 0;
@@ -58,15 +82,85 @@ int playingAgainstComputer()
 	return 0;
 }
 
+void playerOnePlay()
+{
+	bool turnOver = false;
+	SDL_Event event;
+
+	highlightPossibleMoves(p1X, p1Y);
+
+	while (!turnOver)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			if (p1X - moveLeftRight >= p1XS - (4 * moveLeftRight))
+				if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > p1X - moveLeftRight && event.motion.x < p1X - moveLeftRight + squareL && event.motion.y > p1Y && event.motion.y < p1Y + squareL)
+					p1X = p1X - moveLeftRight, turnOver = true;
+
+			if (p1X + moveLeftRight <= p1XS + (4 * moveLeftRight))
+				if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > p1X + moveLeftRight && event.motion.x < p1X + moveLeftRight + squareL && event.motion.y > p1Y && event.motion.y < p1Y + squareL)
+					p1X = p1X + moveLeftRight, turnOver = true;
+
+			if (p1Y + moveUpDown <= p1YS)
+				if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > p1X && event.motion.x < p1X + squareL && event.motion.y > p1Y + moveUpDown && event.motion.y < p1Y + moveUpDown + squareL)
+					p1Y = p1Y + moveUpDown, turnOver = true;
+
+			if (p1Y - moveUpDown >= p2YS)
+				if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > p1X && event.motion.x < p1X + squareL && event.motion.y > p1Y - moveUpDown && event.motion.y < p1Y - moveUpDown + squareL)
+					p1Y = p1Y - moveUpDown, turnOver = true;
+		}
+	}
+
+	playerOneTurn = false;
+}
+
+void playerTwoPlay()
+{
+		bool turnOver = false;
+		SDL_Event event;
+
+		highlightPossibleMoves(p2X, p2Y);
+
+		while (!turnOver)
+		{
+			while (SDL_PollEvent(&event))
+			{
+				if (p2X - moveLeftRight >= p1XS - (4 * moveLeftRight))
+				if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > p2X - moveLeftRight && event.motion.x < p2X - moveLeftRight + squareL && event.motion.y > p2Y && event.motion.y < p2Y + squareL)
+					p2X = p2X - moveLeftRight, turnOver = true;
+
+				if (p2X + moveLeftRight <= p1XS + (4 * moveLeftRight))
+				if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > p2X + moveLeftRight && event.motion.x < p2X + moveLeftRight + squareL && event.motion.y > p2Y && event.motion.y < p2Y + squareL)
+					p2X = p2X + moveLeftRight, turnOver = true;
+
+				if (p2Y + moveUpDown <= p1YS)
+				if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > p2X && event.motion.x < p2X + squareL && event.motion.y > p2Y + moveUpDown && event.motion.y < p2Y + moveUpDown + squareL)
+					p2Y = p2Y + moveUpDown, turnOver = true;
+
+				if (p2Y - moveUpDown >= p2YS)
+				if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > p2X && event.motion.x < p2X + squareL && event.motion.y > p2Y - moveUpDown && event.motion.y < p2Y - moveUpDown + squareL)
+					p2Y = p2Y - moveUpDown, turnOver = true;
+			}
+		}
+	playerOneTurn = true;
+}
+
 int playingAgainstHuman()
 {
 	SDL_Event event;
 	SDL_SetRenderDrawColor(mainRenderer, 255, 255, 255, 255);
+	
+	p1X = p1XS, p1Y = p1YS, p2X = p2XS, p2Y = p2YS;
 
 	while (isRunning)
 		while (SDL_PollEvent(&event))
 		{
-			createPlayTable(event);
+			createPlayTable();
+
+			if (playerOneTurn)
+				playerOnePlay();
+			else
+				playerTwoPlay();
 
 			if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > 700 && event.motion.x < 770 && event.motion.y > 550 && event.motion.y < 590)
 				return 0;
@@ -79,7 +173,6 @@ int playingAgainstHuman()
 
 			SDL_RenderPresent(mainRenderer);
 		}
-
 	return 0;
 }
 

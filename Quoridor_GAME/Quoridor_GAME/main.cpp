5ad2fca8b6ba34db871e091c5074b6fa6f0c4371
gabrielxzc <codrincojocaru@yahoo.com>
2016-12-25 13:37:40 +0200

    Full functional menu, only setups

--- Quoridor_GAME/Quoridor_GAME/main.cpp
+++ Quoridor_GAME/Quoridor_GAME/main.cpp
@@ -14,97 +14,61 @@ void addImageToRenderer(const char *file,int x, int y, int w, int h)
 	SDL_Surface *surface;
 	SDL_Texture *texture;
 	SDL_Rect imageSize = { x, y, w, h };
+
 	surface = IMG_Load(file);
 	texture = SDL_CreateTextureFromSurface(mainRenderer, surface);
 	SDL_RenderCopy(mainRenderer, texture, nullptr, &imageSize);
+
 	SDL_FreeSurface(surface);
 	SDL_DestroyTexture(texture);
 }
 
-void createPlayTable()
+void createPlayTable(SDL_Event event)
 {
 	SDL_SetRenderDrawColor(mainRenderer, 255, 255, 255, 255);
 
 	SDL_RenderClear(mainRenderer);
 
-	SDL_RenderPresent(mainRenderer);
+	addImageToRenderer("images/gameBoard.JPG", 0, 0, 800, 600);
+	addImageToRenderer("images/backButton.PNG", 700, 550, 70, 40);
 
+	SDL_RenderPresent(mainRenderer);
 }
 
 int playingAgainstComputer()
 {
 	SDL_Event event;
-
-	createPlayTable();
+	SDL_SetRenderDrawColor(mainRenderer, 255, 255, 255, 255);
 
 	while (isRunning)
-	{
 		while (SDL_PollEvent(&event))
 		{
+			createPlayTable(event);
+
+			if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > 700 && event.motion.x < 770 && event.motion.y > 550 && event.motion.y < 590)
+				return 0;
+
 			if (event.type == SDL_QUIT)
 			{
 				isRunning = false;
 				return 0;
 			}
 		}
-	}
+
 	return 0;
 }
 
 int playingAgainstHuman()
 {
 	SDL_Event event;
-
-	createPlayTable();
-
-	while (isRunning)
-	{
-		while (SDL_PollEvent(&event))
-		{
-			if (event.type == SDL_QUIT)
-			{
-				isRunning = false;
-				return 0;
-			}
-		}
-	}
-	return 0;
-}
-
-int runStartGameMenu()
-{
-	SDL_Event event;
-	SDL_SetRenderDrawColor(mainRenderer, 255, 0, 0, 255);
+	SDL_SetRenderDrawColor(mainRenderer, 255, 255, 255, 255);
 
 	while (isRunning)
-	{
 		while (SDL_PollEvent(&event))
 		{
-			SDL_RenderClear(mainRenderer);
+			createPlayTable(event);
 
-			addImageToRenderer("images/mainMenuBackground.JPG", 0, 0, 800, 600);
-			addImageToRenderer("images/backButton.PNG", 365, 550, 70, 40);
-
-			if (event.motion.x > 95 && event.motion.x < 395 && event.motion.y > 125 && event.motion.y < 525)
-				addImageToRenderer("images/computerColor.png", 95, 125, 300, 400);
-			else
-				addImageToRenderer("images/computerNonColor.PNG", 95, 125, 300, 400);
-
-			if (event.motion.x > 405 && event.motion.x < 705 && event.motion.y > 125 && event.motion.y < 525)
-				addImageToRenderer("images/vsPlayerColor.PNG", 405, 125, 300, 400);
-			else
-				addImageToRenderer("images/vsPlayerNonColor.PNG", 405, 125, 300, 400);
-
-			if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > 95 && event.motion.x < 395 && event.motion.y > 125 && event.motion.y < 525) {
-				playingAgainstComputer();
-				return 0;
-			}
-			if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > 405 && event.motion.x < 705 && event.motion.y > 125 && event.motion.y < 525) {
-				playingAgainstHuman();
-				return 0;
-			}
-
-			if ((event.type == SDL_MOUSEBUTTONDOWN && (event.motion.x > 365 && event.motion.x < 435 && event.motion.y > 550 && event.motion.y < 590)) || event.key.keysym.sym == SDLK_ESCAPE)
+			if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > 700 && event.motion.x < 770 && event.motion.y > 550 && event.motion.y < 590)
 				return 0;
 
 			if (event.type == SDL_QUIT)
@@ -115,23 +79,81 @@ int runStartGameMenu()
 
 			SDL_RenderPresent(mainRenderer);
 		}
-	}
+
 	return 0;
+}
+
+void createStartGameMenu(SDL_Event event)
+{
+	SDL_RenderClear(mainRenderer);
+
+	addImageToRenderer("images/mainMenuBackground.JPG", 0, 0, 800, 600);
+	addImageToRenderer("images/backButton.PNG", 365, 550, 70, 40);
+
+	if (event.motion.x > 95 && event.motion.x < 395 && event.motion.y > 125 && event.motion.y < 525)
+		addImageToRenderer("images/computerColor.png", 95, 125, 300, 400);
+	else
+		addImageToRenderer("images/computerNonColor.PNG", 95, 125, 300, 400);
+
+	if (event.motion.x > 405 && event.motion.x < 705 && event.motion.y > 125 && event.motion.y < 525)
+		addImageToRenderer("images/vsPlayerColor.PNG", 405, 125, 300, 400);
+	else
+		addImageToRenderer("images/vsPlayerNonColor.PNG", 405, 125, 300, 400);
+
+	SDL_RenderPresent(mainRenderer);
+}
+
+int runStartGameMenu()
+{
+	SDL_Event event;
+	SDL_SetRenderDrawColor(mainRenderer, 255, 255, 255, 255);
+
+	while (isRunning)
+		while (SDL_PollEvent(&event)) 
+		{
+			createStartGameMenu(event);
+
+			if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > 95 && event.motion.x < 395 && event.motion.y > 125 && event.motion.y < 525) 
+			{
+				playingAgainstComputer();
+				return 0;
+			}
+			if (event.type == SDL_MOUSEBUTTONDOWN && event.motion.x > 405 && event.motion.x < 705 && event.motion.y > 125 && event.motion.y < 525) 
+			{
+				playingAgainstHuman();
+				return 0;
+			}
+			if ((event.type == SDL_MOUSEBUTTONDOWN && (event.motion.x > 365 && event.motion.x < 435 && event.motion.y > 550 && event.motion.y < 590)) || event.key.keysym.sym == SDLK_ESCAPE)
+				return 0;
+
+			if (event.type == SDL_QUIT) 
+			{
+				isRunning = false;
+				return 0;
+			}
+		}
+	return 0;
+}
+
+void createInstructionsMenu(SDL_Event event)
+{
+	SDL_RenderClear(mainRenderer);
+
+	addImageToRenderer("images/mainMenuBackground.JPG", 0, 0, 800, 600);
+	addImageToRenderer("images/backButton.PNG", 365, 550, 70, 40);
+
+	SDL_RenderPresent(mainRenderer);
 }
 
 int runInstructionsMenu()
 {
 	SDL_Event event;
-	SDL_SetRenderDrawColor(mainRenderer, 255, 0, 0, 255);
+	SDL_SetRenderDrawColor(mainRenderer, 255, 255, 255, 255);
 
 	while (isRunning)
-	{
 		while (SDL_PollEvent(&event))
 		{
-			SDL_RenderClear(mainRenderer);
-
-			addImageToRenderer("images/mainMenuBackground.JPG", 0, 0, 800, 600);
-			addImageToRenderer("images/backButton.PNG", 365, 550, 70, 40);
+			createInstructionsMenu(event);
 
 			if (event.type == SDL_MOUSEBUTTONDOWN && (event.motion.x > 365 && event.motion.x < 435 && event.motion.y > 550 && event.motion.y < 590))
 				return 0;
@@ -140,11 +162,32 @@ int runInstructionsMenu()
 				isRunning = false;
 				return 0;
 			}
-			SDL_RenderPresent(mainRenderer);
 		}
-	}
-
 	return 0;
+}
+
+void createMainMenu(SDL_Event event)
+{
+	SDL_RenderClear(mainRenderer);
+
+	addImageToRenderer("images/mainMenuBackground.JPG", 0, 0, 800, 600);
+
+	if (event.motion.x > 225 && event.motion.x < 575 && event.motion.y > 175 && event.motion.y < 275)
+		addImageToRenderer("images/startGameHIGHLIGHT.PNG", 225, 175, 350, 100);
+	else
+		addImageToRenderer("images/startGame.PNG", 225, 175, 350, 100);
+
+	if (event.motion.x > 225 && event.motion.x < 575 && event.motion.y > 300 && event.motion.y < 400)
+		addImageToRenderer("images/rulesHIGHLIGHT.PNG", 225, 300, 350, 100);
+	else
+		addImageToRenderer("images/rules.PNG", 225, 300, 350, 100);
+
+	if (event.motion.x > 225 && event.motion.x < 575 && event.motion.y > 425 && event.motion.y < 525)
+		addImageToRenderer("images/exitButtonHIGHLIGHT.PNG", 225, 425, 350, 100);
+	else
+		addImageToRenderer("images/exitButton.PNG", 225, 425, 350, 100);
+
+	SDL_RenderPresent(mainRenderer);
 }
 
 int runMainMenu()
@@ -153,27 +196,9 @@ int runMainMenu()
 	SDL_SetRenderDrawColor(mainRenderer, 255, 255, 255, 255);
 
 	while (isRunning)
-	{
 		while (SDL_PollEvent(&event))
 		{
-			SDL_RenderClear(mainRenderer);
-
-			addImageToRenderer("images/mainMenuBackground.JPG", 0, 0, 800, 600);
-
-			if (event.motion.x > 225 && event.motion.x < 575 && event.motion.y > 175 && event.motion.y < 275)
-				addImageToRenderer("images/startGameHIGHLIGHT.PNG", 225, 175, 350, 100);
-			else
-				addImageToRenderer("images/startGame.PNG", 225, 175, 350, 100);
-
-			if (event.motion.x > 225 && event.motion.x < 575 && event.motion.y > 300 && event.motion.y < 400)
-				addImageToRenderer("images/rulesHIGHLIGHT.PNG", 225, 300, 350, 100);
-			else
-				addImageToRenderer("images/rules.PNG", 225, 300, 350, 100);
-
-			if (event.motion.x > 225 && event.motion.x < 575 && event.motion.y > 425 && event.motion.y < 525)
-				addImageToRenderer("images/exitButtonHIGHLIGHT.PNG", 225, 425, 350, 100);
-			else
-				addImageToRenderer("images/exitButton.PNG", 225, 425, 350, 100);
+			createMainMenu(event);
 
 			if (event.type == SDL_MOUSEBUTTONDOWN && (event.motion.x > 225 && event.motion.x < 575 && event.motion.y > 175 && event.motion.y < 275))
 				runStartGameMenu();
@@ -181,11 +206,7 @@ int runMainMenu()
 				runInstructionsMenu();
 			if (event.type == SDL_QUIT || event.type == SDL_MOUSEBUTTONDOWN && (event.motion.x > 225 && event.motion.x < 575 && event.motion.y > 425 && event.motion.y < 525))
 				isRunning = false;
-
-			SDL_RenderPresent(mainRenderer);
 		}
-	}
-
 	return 0;
 }
 
@@ -206,6 +227,7 @@ int main(int argc, char* argv[])
 
 	runMainMenu();
 
+	SDL_DestroyRenderer(mainRenderer);
 	SDL_DestroyWindow(mainWindow);
 	TTF_Quit();
 	IMG_Quit();
